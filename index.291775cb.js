88ff9c35807f8aa60e1a414d1646fa195775a0d4
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-09-20 16:26:59 +0300

    make build

---
+++ index.291775cb.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var t=document.createElement("label"),l=e.parentElement;t.className="field-label",t.setAttribute("for",e.id),t.textContent=e.name,e.setAttribute("placeholder",e.name.toUpperCase()),l.appendChild(t)});
+//# sourceMappingURL=index.291775cb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    const parentElement = input.parentElement;
+    label.className = "field-label";
+    label.setAttribute("for", input.id);
+    label.textContent = input.name;
+    input.setAttribute("placeholder", input.name.toUpperCase());
+    parentElement.appendChild(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
